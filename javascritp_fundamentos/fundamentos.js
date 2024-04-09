a5f55a1fd58e0b118c4ec38f09f0eda26e9960c8
Jorgeluis02 <jorgeluisoter18@gmail.com>
2024-04-09 18:41:55 -0500

    fundamentos-js1

---
+++ javascritp_fundamentos/fundamentos.js
@@ -0,0 +1,34 @@
+// `var numero = 10;
+// if (numero > 0) {
+//   console.log('El número es positivo');
+// } else if (numero === 0) {
+//   console.log('El número es cero');
+// } else {
+//   console.log('El número es negativo');
+// }
+
+// var i;
+// for (i = 0; i < 5; i++) {
+//   console.log('Iteración ' + (i + 1));
+// }
+
+// var contador = 0;
+// while (contador < 3) {
+//   console.log('Iteración ' + (contador + 1));
+//   contador++;
+// }
+
+
+var numero = -1;
+if (numero > 0) {
+  console.log('el numero es positivo');
+} else if (numero === 0) {
+     console.log('el numero es negativo')
+}  else {
+     console.log('elnumero es negativo')
+}
+
+let i;
+for (i = 0; i < 5; i++) {
+    console.log('interaccion ' + (i + 1));
+}
